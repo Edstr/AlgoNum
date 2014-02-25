35c9c274f305ad9ef0e106478e321b97bd06e421
Edstr <ed.strambini@gmail.com>
2014-02-25 15:16:44 +0100

    Ajout de la classe "codage17bits"
    et de la saisie des double

--- Labo1/main.cpp
+++ Labo1/main.cpp
@@ -1,9 +1,36 @@
+/*
+    Date: 25.02.2014
+    Auteurs: Eddy Strambini, Cyriaque Skrapits, Da Mota Marques Fabio Manuel
+*/
+
 #include <iostream>
+#include <limits>
 
 using namespace std;
 
+double saisie(string texte)
+{
+    double nombre;
+
+    cout << texte;
+
+    while (!(cin >> nombre))
+    {
+        cerr << "Erreur de saisie.\n";
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << texte;
+    }
+
+    return nombre;
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    double nmb1, nmb2;
+
+    nmb1 = saisie("Premier nombre : ");
+    nmb2 = saisie("Deuxieme nombre : ");
+
     return 0;
 }
---
+++ Labo1/src/codage17bits.cpp
@@ -0,0 +1,11 @@
+#include "codage17bits.h"
+
+codage17bits::codage17bits()
+{
+    //ctor
+}
+
+codage17bits::~codage17bits()
+{
+    //dtor
+}
