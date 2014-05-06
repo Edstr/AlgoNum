a4021bb57d58401066aba1982012cba0b74c1c49
Cyriaque Skrapits <cysoun@gmail.com>
2014-05-06 15:10:15 +0200

    Correction du CSV (Labo 4) au cas où G++ ne supporte pas C++11.

--- Labo4/partie2/or/CSV.cpp
+++ Labo4/partie2/or/CSV.cpp
@@ -22,10 +22,18 @@ CSV::CSV(const std::string &fileName)
 	float value;
 
 	// Récupère chaque ligne et en ressort les données.
+
 	while(std::getline(file, buffer))
 	{
-		key = stoi(buffer.substr(0, 4));
-		value = stof(buffer.substr(5, buffer.length() - 5));
+	    // Méthode C++11
+		//key = stoi(buffer.substr(0, 4));
+		//value = stof(buffer.substr(5, buffer.length() - 5));
+
+        // Workaround si C++11 n'est pas supporté par G++.
+		std::stringstream ssKey(buffer.substr(0, 4));
+	    std::stringstream ssValue(buffer.substr(5, buffer.length() - 5));
+        ssKey >> key;
+        ssValue >> value;
 
 		values[key] = value;
 	}
